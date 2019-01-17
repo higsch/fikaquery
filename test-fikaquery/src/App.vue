5c04004c1cde6a331095cfdf991c25e5c66f98df
Matthias Stahl <matthias.stahl87@web.de>
2019-01-17 23:55:14 +0100

    test: added file upload field

--- test-fikaquery/src/App.vue
+++ test-fikaquery/src/App.vue
@@ -1,13 +1,37 @@
 <template>
   <div id="app">
+    <b-container>
+      <h1>fikaquery test</h1>
+      <h5>Quick tester for the merge between database and JSON.</h5>
+      <fika-content />
+    </b-container>
   </div>
 </template>
 
 <script>
+import FikaContent from './components/FikaContent.vue';
+
 export default {
   name: 'app',
+  components: {
+    FikaContent,
+  },
 };
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Asar|Domine');
+
+h1 {
+  font-family: "Domine", serif;
+  font-size: 3rem;
+}
+
+h5 {
+  font-family: "Asar", serif;
+}
+
+#app {
+  padding: 20px 0 0 0;
+}
 </style>
---
+++ test-fikaquery/src/components/FikaContent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="content-wrapper">
+    <b-form>
+      <b-form-file v-model="file"
+                   :state="Boolean(file)"
+                   placeholder="Choose a fikaquery file..."
+                   accept=".json, .JSON"
+                   class="file-input">
+      </b-form-file>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FikaContent',
+  data() {
+    return {
+      file: null,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.file-input {
+  max-width: 500px;
+  margin: 20px 0 0 0;
+}
+</style>
