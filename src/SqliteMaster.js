0c618f977238b3dd3ba4dab8b647929628e216e1
Matthias Stahl <matthias.stahl87@web.de>
2019-02-05 21:57:00 +0100

    parse index col names

--- src/SqliteMaster.js
+++ src/SqliteMaster.js
@@ -36,7 +36,7 @@ const SqliteMaster = class {
     allCells.forEach((cell) => {
       if (cell[COL.TYPE] === 'index') {
         this._indices = {
-          [cell[COL.TBL_NAME]]: this.makeCellObj(cell),
+          [cell[COL.NAME]]: this.makeCellObj(cell),
           ...this._indices,
         };
       }
@@ -47,29 +47,36 @@ const SqliteMaster = class {
         };
       }
     });
-    console.log(this._tables);
   }
 
   makeCellObj(cell) {
     return {
+      tblName: cell[COL.TBL_NAME],
       name: cell[COL.NAME],
+      type: cell[COL.TYPE],
       rootPage: cell[COL.ROOTPAGE],
       sql: cell[COL.SQL],
-      cols: this.parseColsFromSQL(cell[COL.SQL]),
+      [cell[COL.TYPE] === 'table' ? 'cols' : 'on']: this.parseColsFromSQL(cell),
     };
   }
 
-  parseColsFromSQL(sql) {
+  parseColsFromSQL(cell) {
     let cols = [];
-    const r = /([\s(][A-Za-z0-9_]+\s[A-Z_\s]+[,)])/g;
-    if (sql) {
-      const match = sql.match(r);
+    if (cell[COL.SQL]) {
+      let r = '';
+      if (cell[COL.TYPE] === 'index') {
+        r = /\([A-Za-z_]+\)/g;
+      }
+      if (cell[COL.TYPE] === 'table') {
+        r = /[\s(][A-Za-z0-9_]+\s[A-Z_\s]+[,)]/g;
+      }
+      const match = cell[COL.SQL].match(r);
       if (match) {
         cols = match.map((e) => {
           const sub = e.slice(1, -1);
           return {
             name: sub.split(' ')[0],
-            type: sub.split(' ')[1],
+            type: sub.split(' ')[1] || null,
           };
         });
       }
